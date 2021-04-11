f46fbc5d8b89dd9756402c240f5ca952b9fae8be
garima.mittal <garima.mittal@thoughtworks.com>
2021-04-11 12:57:27 +0530

    Garima - Brightest Star. Test Cases

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,11 +1,63 @@
-import { beforeAll, expect, it } from "@jest/globals";
+import { beforeAll, expect, it, jest } from "@jest/globals";
 import { BrightestStar } from "../src";
 
+const responseMock = {
+  signature: { source: "NASA/JPL Fireball Data API", version: "1.0" },
+  count: "4",
+  fields: [
+    "date",
+    "energy",
+    "impact-e",
+    "lat",
+    "lat-dir",
+    "lon",
+    "lon-dir",
+    "alt",
+    "vel",
+  ],
+  data: [
+    [
+      "2021-04-02 15:52:58",
+      "13.7",
+      "0.4",
+      "71.2",
+      "N",
+      "106.7",
+      "E",
+      "40.0",
+      "-",
+    ],
+    [
+      "2021-03-06 08:43:06",
+      "14.1",
+      "0.41",
+      "48.6",
+      "S",
+      "90.4",
+      "E",
+      "31.1",
+      "-",
+    ],
+    [
+      "2021-03-05 13:50:01",
+      "3.9",
+      "0.13",
+      "81.1",
+      "S",
+      "141.1",
+      "E",
+      "32.5",
+      "-",
+    ],
+    ["2021-02-28 03:47:37", "2.8", "0.098", "9.2", "N", "64.1", "W", null, "-"],
+  ],
+};
+
 describe("Brightest Start ", () => {
-    let brightestStar;
-    beforeAll(()=>{
-        brightestStar=  new BrightestStar();
-    })
+  let brightestStar;
+  beforeAll(() => {
+    brightestStar = new BrightestStar();
+  });
   it("should convert to signed Coordinates ", () => {
     const coordinateDetails = [51.0, "S", 21.1, "W"];
     const expectedSignedCoordinate = {
@@ -13,7 +65,7 @@ describe("Brightest Start ", () => {
       lat: -51,
       lon: -21.1,
     };
-    const actualSignedCoordinate =  brightestStar.convertToSignedCoordinates(
+    const actualSignedCoordinate = brightestStar.convertToSignedCoordinates(
       coordinateDetails,
       1,
       0,
@@ -57,13 +109,42 @@ describe("Brightest Start ", () => {
     const list = [];
     brightestStar.addBrightestStarForOneLocation(list, brightStar);
 
-    expect(list.length===1);
+    expect(list.length === 1);
   });
 
-//   it("should call addBrightestStarForOneLocation for all four locations inside filterListFindBrightestStar",()=>{
-//     brightestStar.filterListFindBrightestStar();
-//       expect(brightestStar.fireball).toHaveBeenCalledTimes(4);
-//       expect(brightestStar.addBrightestStarForOneLocation).toHaveBeenCalledTimes(4);
-//       expect(Brightest.filterCoordinatesInRangeAndMaxEnergyStar).toHaveBeenCalledTimes(4);
-//   })
+  it("should call addBrightestStarForOneLocation for all four locations inside filterListFindBrightestStar", () => {
+    const list = [
+      { lat: "71.2", lon: "106.7", energy: "106.7" },
+      { lat: -48.6, lon: "90.4", energy: "90.4" },
+      { lat: -81.1, lon: "141.1", energy: "141.1" },
+      { lat: "9.2", lon: -64.1, energy: "64.1" },
+    ];
+    const spyOnfireball = jest.spyOn(brightestStar, "fireball");
+    const spyOnaddBrightestStarForOneLocation = jest.spyOn(
+      brightestStar,
+      "addBrightestStarForOneLocation"
+    );
+    const spyOnfilterCoordinatesInRangeAndMaxEnergyStar = jest.spyOn(
+      brightestStar,
+      "filterCoordinatesInRangeAndMaxEnergyStar"
+    );
+
+    brightestStar.filterListFindBrightestStar(list);
+
+    expect(spyOnfireball).toHaveBeenCalledTimes(4);
+    expect(spyOnaddBrightestStarForOneLocation).toHaveBeenCalledTimes(4);
+    expect(spyOnfilterCoordinatesInRangeAndMaxEnergyStar).toHaveBeenCalledTimes(
+      4
+    );
+  });
+  it("should test the init function fetch call", async () => {
+    await brightestStar.init();
+    jest
+      .spyOn(brightestStar, "mapToSignedCoordinateList")
+      .mockReturnValue(jest.fn());
+    setTimeout(() => {
+      expect(fetch).toHaveBeenCalled();
+      expect(brightestStar.mapToSignedCoordinateList).toHaveBeenCalled();
+    }, 1000);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,97 +1,137 @@
-const expectExport = require("expect");
 const fetch = require("node-fetch");
-function BrightestStar(){
-  const BOSTON = { latitude: 42.36, longitude: -71.05};
-  const LONDON = { latitude: 51.51, longitude: -0.12};
-  const NCR = {latitude: 28.58, longitude: 77.31};
- const SANFRANCISCO ={latitude: 37.79, longitude: -122.40};
+function BrightestStar() {
+  const BOSTON = { latitude: 42.36, longitude: -71.05 };
+  const LONDON = { latitude: 51.51, longitude: -0.12 };
+  const NCR = { latitude: 28.58, longitude: 77.31 };
+  const SANFRANCISCO = { latitude: 37.79, longitude: -122.4 };
 
-  this.init= function(){
-    try{
-      fetch('https://ssd-api.jpl.nasa.gov/fireball.api?date-min=2017-01-01&req-loc=true&limit=4').then(response => {
-        return response.json();
-      }).then(response => this.mapToSignedCoordinateList(response)).then(list => {
-        const brightestStar = this.filterListFindBrightestStar(list);
-        console.log("Nasa Nasa on the Earth who's the Brightest Star of them all!!! \n\n Star with latitude", brightestStar.lat, " and longitude ", brightestStar.lon, " is the brightest of them all!!! \n Energy that is high as ", brightestStar.energy, " joules");
-      });
-    }
-    catch(ex){
+  this.init = async function () {
+    try {
+      const response = await fetch(
+        "https://ssd-api.jpl.nasa.gov/fireball.api?date-min=2017-01-01&req-loc=true&limit=4"
+      );
+      const list = this.mapToSignedCoordinateList(await response.json());
+      const brightestStar = this.filterListFindBrightestStar(list);
+      if (brightestStar)
+        console.log(
+          "Nasa Nasa on the Earth who's the Brightest Star of them all!!! \n\n Star with latitude",
+          brightestStar.lat,
+          " and longitude ",
+          brightestStar.lon,
+          " is the brightest of them all!!! \n Energy that is high as ",
+          brightestStar.energy,
+          " joules"
+        );
+    } catch (ex) {
       console.error(ex);
     }
-    }
-  this.mapToSignedCoordinateList= function(response={field:[],data:[]}){
-    const {fields,data}= response;
-    let latIndex = fields.indexOf('lat');
-    let latDirIndex = fields.indexOf('lat-dir');
-    let lonIndex = fields.indexOf('lon');
-    let lonDirIndex = fields.indexOf('lon-dir');
-    let signedCoordinatesEnergyList =[];
-    
-      data.forEach(coordinateDetails=>{
-      signedCoordinatesEnergyList.push(this.convertToSignedCoordinates(coordinateDetails, latDirIndex, latIndex, lonDirIndex, lonIndex));
-      }); 
-    
-    return signedCoordinatesEnergyList;
-  }
-    this.filterListFindBrightestStar= function(list){
-      let brightestStars=[];
-      this.addBrightestStarForOneLocation(brightestStars,this.filterCoordinatesInRangeAndMaxEnergyStar(list,this.fireball( BOSTON.latitude,BOSTON.longitude)));
-      this.addBrightestStarForOneLocation(brightestStars,this.filterCoordinatesInRangeAndMaxEnergyStar(list,this.fireball( LONDON.latitude,LONDON.longitude)))
-      this.addBrightestStarForOneLocation(brightestStars,this.filterCoordinatesInRangeAndMaxEnergyStar(list,this.fireball( NCR.latitude,NCR.longitude)));
-      this.addBrightestStarForOneLocation(brightestStars,this.filterCoordinatesInRangeAndMaxEnergyStar(list,this.fireball( SANFRANCISCO.latitude,SANFRANCISCO.longitude)));
-    
-      return brightestStars.sort((a,b)=>b.energy-a.energy).shift();
-    }
-    this.addBrightestStarForOneLocation= function(inputArray,fn){
-      inputArray.push(fn);
-    }
-    this.mathAbs= function(value){
-      return Math.abs(value);
-    }
-    this.fireball= function( lat,lon){
-      const minLat=   this.mathAbs(lat)-15;
-      const maxLat =  this.mathAbs(lat)+15;
-      const minLon =  this.mathAbs(lon)-15;
-      const maxLon =  this.mathAbs(lon)+15;    
-      const latRange = lat>0?{min:minLat,max:maxLat}:{min:-minLat,max:maxLat};
-      const lonRange = lon>0?{min:minLon,max:maxLon}:{min:-minLon,max:-maxLon}
-      return {latRange,lonRange};
-    }
-    
-    this.convertToSignedCoordinates=function(coordinateDetails, latDirIndex, latIndex, lonDirIndex, lonIndex) {
-      let mapedPosition = {};
-    
-      if (coordinateDetails[latDirIndex] === 'S')
-        mapedPosition.lat = -coordinateDetails[latIndex];
-    
-      if (coordinateDetails[latDirIndex] === 'N')
-        mapedPosition.lat = coordinateDetails[latIndex];
-    
-      if (coordinateDetails[lonDirIndex] === 'W')
-        mapedPosition.lon = -coordinateDetails[lonIndex];
-    
-      if (coordinateDetails[lonDirIndex] === 'E')
-        mapedPosition.lon = coordinateDetails[lonIndex];
-    
-    
-      mapedPosition.energy = coordinateDetails[lonIndex];
-      return mapedPosition;
-    }
-    
-  this.filterCoordinatesInRangeAndMaxEnergyStar= function(list,latlonRange){
-      let inRangeCoordinatesList= [];
-      let brightestStarForOneCoordinate;
-      list.map(locationAndEnergy=>{
-        if((locationAndEnergy.lat >= latlonRange.latRange.min && locationAndEnergy.lat <= latlonRange.latRange.max) ||
-          (locationAndEnergy.lon >= latlonRange.lonRange.min && locationAndEnergy.lon <= latlonRange.lonRange.max)
+  };
+  this.mapToSignedCoordinateList = function (
+    response = { field: [], data: [] }
+  ) {
+    const { fields, data } = response;
+    let latIndex = fields.indexOf("lat");
+    let latDirIndex = fields.indexOf("lat-dir");
+    let lonIndex = fields.indexOf("lon");
+    let lonDirIndex = fields.indexOf("lon-dir");
+    let signedCoordinatesEnergyList = [];
+
+    data.forEach((coordinateDetails) => {
+      signedCoordinatesEnergyList.push(
+        this.convertToSignedCoordinates(
+          coordinateDetails,
+          latDirIndex,
+          latIndex,
+          lonDirIndex,
+          lonIndex
         )
-          inRangeCoordinatesList.push(locationAndEnergy);
-      })
-         if(inRangeCoordinatesList.length)
-           brightestStarForOneCoordinate = inRangeCoordinatesList.sort((a,b)=>b.energy - a.energy).shift();
-         return brightestStarForOneCoordinate;
-    }
-};
-const brightestStarInstance= new BrightestStar();
+      );
+    });
+
+    return signedCoordinatesEnergyList;
+  };
+  this.filterListFindBrightestStar = function (list) {
+    let brightestStars = [];
+    [BOSTON, LONDON, NCR, SANFRANCISCO].forEach((location) => {
+      this.addBrightestStarForOneLocation(
+        brightestStars,
+        this.filterCoordinatesInRangeAndMaxEnergyStar(
+          list,
+          this.fireball(location.latitude, location.longitude)
+        )
+      );
+    });
+
+    return brightestStars.sort((a, b) => b.energy - a.energy)[0];
+  };
+  this.addBrightestStarForOneLocation = function (inputArray, fn) {
+    inputArray.push(fn);
+  };
+  this.mathAbs = function (value) {
+    return Math.abs(value);
+  };
+  this.fireball = function (lat, lon) {
+    const minLat = this.mathAbs(lat) - 15;
+    const maxLat = this.mathAbs(lat) + 15;
+    const minLon = this.mathAbs(lon) - 15;
+    const maxLon = this.mathAbs(lon) + 15;
+    const latRange =
+      lat > 0 ? { min: minLat, max: maxLat } : { min: -minLat, max: maxLat };
+    const lonRange =
+      lon > 0 ? { min: minLon, max: maxLon } : { min: -minLon, max: -maxLon };
+    return { latRange, lonRange };
+  };
+
+  this.convertToSignedCoordinates = function (
+    coordinateDetails,
+    latDirIndex,
+    latIndex,
+    lonDirIndex,
+    lonIndex
+  ) {
+    let mapedPosition = {};
+
+    if (coordinateDetails[latDirIndex] === "S")
+      mapedPosition.lat = -coordinateDetails[latIndex];
+
+    if (coordinateDetails[latDirIndex] === "N")
+      mapedPosition.lat = coordinateDetails[latIndex];
+
+    if (coordinateDetails[lonDirIndex] === "W")
+      mapedPosition.lon = -coordinateDetails[lonIndex];
+
+    if (coordinateDetails[lonDirIndex] === "E")
+      mapedPosition.lon = coordinateDetails[lonIndex];
+
+    mapedPosition.energy = coordinateDetails[lonIndex];
+    return mapedPosition;
+  };
+
+  this.filterCoordinatesInRangeAndMaxEnergyStar = function (list, latlonRange) {
+    let inRangeCoordinatesList = [];
+    let brightestStarForOneCoordinate;
+    list.map((locationAndEnergy) => {
+      if (
+        locationAndEnergy.lat >= latlonRange.latRange.min &&
+        locationAndEnergy.lat <= latlonRange.latRange.max &&
+        locationAndEnergy.lon >= latlonRange.lonRange.min &&
+        locationAndEnergy.lon <= latlonRange.lonRange.max
+      )
+        if (!locationAndEnergy.lat || !locationAndEnergy.lon) {
+          console.log(locationAndEnergy, "---8-----8------");
+        }
+      if (locationAndEnergy && locationAndEnergy.lat && locationAndEnergy.lon) {
+        inRangeCoordinatesList.push(locationAndEnergy);
+      }
+    });
+
+    if (inRangeCoordinatesList.length)
+      brightestStarForOneCoordinate = inRangeCoordinatesList
+        .sort((a, b) => b.energy - a.energy)
+        .shift();
+    return brightestStarForOneCoordinate;
+  };
+}
+const brightestStarInstance = new BrightestStar();
 brightestStarInstance.init();
-module.exports={BrightestStar};
+module.exports = { BrightestStar };
